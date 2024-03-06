9cad2157cb829762ee5491eea66b5d68cdf38a34
Msaeed144 <mohammadsaeed76@gmail.com>
2024-03-06 10:58:31 +0330

    create navbar base

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
+import logo from '../assets/logo.png';
+
+import { FcLike } from "react-icons/fc";
+import { TbTransformFilled } from "react-icons/tb";
+import { FcAbout } from "react-icons/fc";
+
+import styles from './Navbar.module.css'
 function Navbar() {
   return (
-    <div>
-        <ul>
+    <div className={styles.container}>
+        <ul className={styles.navbarUl}>
+          <li><img className={styles.logo} src={logo} alt="logo" /></li>
+          <li>مورد علاقه ها<FcLike /></li>
+          
+          <li>تبدیل گر<TbTransformFilled /></li>
+          <li>درباره ما<FcAbout /></li>
         </ul>
     </div>
   )
