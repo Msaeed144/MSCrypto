f82f8c0645e960359bb8416372e3d186404f35e1
Msaeed144 <mohammadsaeed76@gmail.com>
2024-04-23 00:58:11 +0330

    tail config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ export default {
       iranyekan: ["iranyekan"],
       iranyekan1: ["iranyekan2"],
       iranyekan2: ["iranyekan2"],
+      Estedad: ["Estedad"],
+      
     },
 
     extend: {},
