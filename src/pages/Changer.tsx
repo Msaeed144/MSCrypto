204ad9eb8041391909b76b43f6c0e6d1bb5e9a1b
Msaeed144 <mohammadsaeed76@gmail.com>
2024-04-28 18:43:48 +0330

    create chager page

--- src/pages/Changer.tsx
+++ src/pages/Changer.tsx
@@ -1,6 +1,10 @@
+import TransformerBox from "../components/TransformerBox"
+
 function Changer() {
   return (
-    <div>Changer</div>
+    <div className="flex flex-col	justify-center items-center">
+        <TransformerBox />
+    </div>
   )
 }
 
